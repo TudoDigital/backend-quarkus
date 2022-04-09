78b5655ccee56ae72aad996488d3ed91a6127431
RCLMacedo07 <ruan.macedo@fatec.sp.gov.br>
2022-04-09 16:43:11 -0300

    Criando o mecanismo que aplica as promoções

--- src/main/java/controller/Produto.java
+++ src/main/java/controller/Produto.java
@@ -37,5 +37,89 @@ public class Produto {
 	public int getFk_Categoria_Cat_Id(){
 		return this.fk_categoria_cat_id;
 	} 
+
+	public boolean buscaExistePromocao(int idProduto){
+
+        boolean result = ModelProduto.getExistePromocao(idProduto);
+
+        if(result == true){
+
+            return true;
+
+        }
+
+        else{
+
+            return False;
+
+        }
+
+    }
+
+    public String[] buscaTipoPromocao(int idProduto){
+
+        String[] result = ModelProduto.buscaTipoPromocao(idProduto);
+
+        return result;
+
+    }
+
+    public aplicaDesconto(String tip_descricao, int pro_desconto, int pro_quant_min, int pro_quant_bonus, String pdt_nome, float pdt_preco){
+
+        float valor_final = (pdt_preco * pro_desconto)/100;
+        float valor_descontado = (pdt_preco - valor_final);
+
+        return ("O produto selecionado foi:" + pdt_nome);
+        return ("Valor do Produto:" + pdt_preco);
+        return ("Tipo de desconto aplicado:" + tip_descricao + ", com o desconto de:" + pro_desconto + "%");
+        return ("Valor final com descontos: R$" + valor_final);
+        return ("Valor descontado: -R$" +valor_descontado);
+        
+
+    }
+
+    public aplicaQuantDesconto(String tip_descricao, int pro_desconto, int pro_quant_min, int pro_quant_bonus, String pdt_nome, float pdt_preco){
+
+        int pro_quant_total = pro_quant_min + pro_quant_bonus;
+        float valor_final = pdt_preco * pro_quant_total;
+        float valor_descontado = (pdt_preco * pro_quant_bonus);
+
+        return ("O produto selecionado foi:" + pdt_nome);
+        return ("Valor do Produto:" + pdt_preco);
+        return ("Tipo de desconto aplicado:" + tip_descricao);
+        return ("Valor final com descontos: R$" + valor_final);
+        return ("Quantidade:" + pro_quant_total);
+        return ("Valor descontado: -R$" + valor_descontado);        
+
+    }
+
+    public getPromocoes(int idProduto){
+
+        if (buscaExistePromocao(idProduto) == true){
+
+            String[] result = buscaTipoPromocao(idProduto);
+
+            String tip_descricao = result[0];
+            int pro_desconto = Integer.valueOf(result[1]);
+            int pro_quant_min = Integer.valueOf(result[2]);
+            int pro_quant_bonus = Integer.valueOf(result[3]);
+            String pdt_nome = result[4];
+            Float pdt_preco = Float.parseFloat(result[5]);
+
+            if (tip_descricao == "desconto"){
+
+                aplicaDesconto(tip_descricao, pro_desconto, pro_quant_min, pro_quant_bonus, pdt_nome, pdt_preco);
+
+            }
+
+            if (tip_descricao == "quantitativo"){
+
+                aplicaQuantDesconto(tip_descricao, pro_desconto, pro_quant_min, pro_quant_bonus, pdt_nome, pdt_preco);
+
+            }
+
+        }
+
+    }
 	
 }
